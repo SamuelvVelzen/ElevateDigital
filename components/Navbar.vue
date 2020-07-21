4c0574f956a1dcd695aa33997acabdca3cc8e62f
Samuel <samuelvvelzen@gmail.com>
2020-07-21 16:06:47 +0200

    improved navbar styling

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,7 @@
 <template>
-	<nav class="sticky top-0 p-4 flex justify-around bg-primary items-center">
+	<nav
+		class="sticky top-0 py-4 px-10 sm:px-20 flex justify-around bg-primary items-center"
+	>
 		<span class="text-2xl font-bold">Samuel.</span>
 		<nuxt-link to="#" class="ml-auto">Test</nuxt-link>
 		<nuxt-link to="#" class="ml-4">Test</nuxt-link>
