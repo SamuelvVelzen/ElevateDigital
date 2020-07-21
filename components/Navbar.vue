e1c90c4bbac68ad5cd2918d2e2ca87dd1f67ac72
Samuel <samuelvvelzen@gmail.com>
2020-07-21 10:34:14 +0200

    added navbar component

---
+++ components/Navbar.vue
@@ -0,0 +1,8 @@
+<template>
+	<nav class="sticky top-0 p-4 flex justify-around bg-primary items-center">
+		<span class="text-2xl font-bold">Samuel.</span>
+		<nuxt-link to="#" class="ml-auto">Test</nuxt-link>
+		<nuxt-link to="#" class="ml-4">Test</nuxt-link>
+		<nuxt-link to="#" class="ml-4">Test</nuxt-link>
+	</nav>
+</template>
