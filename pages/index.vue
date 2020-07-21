b2c88935948cc34af727de6ee090284106e90bb0
Samuel <samuelvvelzen@gmail.com>
2020-07-21 10:35:14 +0200

    remove unnecessary code

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,7 @@
 <template>
-	<div class="container">
-		<div>
-			<Logo />
-			<h1 class="title c-blue-800">Samuel</h1>
-			<div class="links">
-				<a
-					href="https://nuxtjs.org/"
-					target="_blank"
-					rel="noopener noreferrer"
-					class="button--green"
-				>
-					Documentation
-				</a>
-				<a
-					href="https://github.com/nuxt/nuxt.js"
-					target="_blank"
-					rel="noopener noreferrer"
-					class="button--grey"
-					>GitHub</a
-				>
-			</div>
-		</div>
-	</div>
+	<article class="h-screen-2">
+		<p>hoi</p>
+	</article>
 </template>
 
 <script lang="ts">
@@ -31,20 +11,6 @@ export default Vue.extend({});
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS */
-.container {
-	@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-
-.container {
-	margin: 0 auto;
-	min-height: 100vh;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	text-align: center;
-}
-
 .title {
 	font-family: "Quicksand", "Source Sans Pro", -apple-system,
 		BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial,
