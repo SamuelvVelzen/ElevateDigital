8eebfb0f2ac62108c8e9230de002dcb7a43a0e93
Samuel <samuelvvelzen@gmail.com>
2020-07-24 11:37:42 +0200

    added clickable methods to change the state of the navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,10 +1,70 @@
 <template>
 	<nav
-		class="sticky top-0 flex justify-around bg-primary items-center px-4 sm:px-16 md:px-20 lg:px-24"
+		class="fixed w-full flex justify-between bg-primary items-center py-3 px-4 sm:px-16 md:px-20 lg:px-16 lg:max-w-3/4 lg:mx-auto z-50 opacity-95 flex-wrap lg:left-1/8"
+		:class="([open ? 'open' : null, closing ? 'closing' : null])"
 	>
 		<span class="text-2xl font-bold">Samuel.</span>
-		<nuxt-link to="#" class="ml-auto">Test</nuxt-link>
-		<nuxt-link to="#" class="ml-4">Test</nuxt-link>
-		<nuxt-link to="#" class="ml-4">Test</nuxt-link>
+		<label
+			class="menu-icon sm:hidden cursor-pointer ml-auto pt-px-10 pb-px-10"
+			@click="mobileNav"
+		>
+			<span class="menu-icon-line block"></span>
+		</label>
+
+		<div
+			class="navLinks sm:block sm:w-auto sm:transition-none border-t-2 border-dark sm:border-t-0"
+			:class="[
+				open ? 'w-full flex justify-between' : '',
+				closing ? 'w-full flex justify-between' : '',
+			]"
+		>
+			<nuxt-link to="#" class="sm:ml-auto navLink">Test</nuxt-link>
+			<nuxt-link to="#" class="ml-4 navLink">Test</nuxt-link>
+			<nuxt-link to="#" class="ml-4 navLink">Test</nuxt-link>
+		</div>
 	</nav>
 </template>
+
+<script>
+export default {
+	data() {
+		return {
+			open: true,
+			closing: false,
+		};
+	},
+
+	mounted() {
+		if (window.innerWidth < 640) {
+			this.open = false;
+		}
+
+		window.addEventListener("resize", this.checkMobileNav);
+	},
+	destroyed() {
+		window.removeEventListener("resize", this.checkMobileNav);
+	},
+	methods: {
+		mobileNav() {
+			if (this.open) {
+				this.open = false;
+
+				this.closing = true;
+
+				setTimeout(() => {
+					this.closing = false;
+				}, 500);
+			} else {
+				this.open = true;
+			}
+		},
+		checkMobileNav() {
+			if (window.innerWidth > 640) {
+				this.open = true;
+			} else {
+				this.open = false;
+			}
+		},
+	},
+};
+</script>
