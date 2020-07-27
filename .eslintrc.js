e38941bd5553e90b9298a2f6d5ae4cbeeff87e8f
Samuel <samuelvvelzen@gmail.com>
2020-07-27 12:36:32 +0200

    improve configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,20 @@ module.exports = {
 	],
 	plugins: ["prettier"],
 	// add your custom rules here
-	rules: {},
+	rules: {
+		"at-rule-no-unknown": [
+			true,
+			{
+				ignoreAtRules: [
+					"tailwind",
+					"apply",
+					"variants",
+					"responsive",
+					"screen",
+				],
+			},
+		],
+		"declaration-block-trailing-semicolon": null,
+		"no-descending-specificity": null,
+	},
 };
