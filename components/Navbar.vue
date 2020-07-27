42d79c12a610084cf580534cef153445e8ce5a88
Samuel <samuelvvelzen@gmail.com>
2020-07-27 16:01:39 +0200

    added animation tag from vue instead of using a timer

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
 	<nav
 		class="fixed w-full flex justify-between bg-primary items-center py-2 px-4 sm:px-16 md:px-20 lg:px-16 lg:max-w-3/4 lg:mx-auto z-50 opacity-95 flex-wrap lg:left-1/8"
-		:class="([open ? 'open' : null, closing ? 'closing' : null])"
+		:class="open ? 'open' : null"
 	>
-		<span class="text-2xl font-bold">Samuel.</span>
+		<nuxt-link to="/" class="text-2xl font-bold">Samuel.</nuxt-link>
 		<label
 			class="menu-icon sm:hidden cursor-pointer ml-auto pt-px-10 pb-px-10"
 			@click="mobileNav"
@@ -11,17 +11,16 @@
 			<span class="menu-icon-line block"></span>
 		</label>
 
-		<div
-			class="navLinks sm:block sm:w-auto sm:transition-none border-t-2 border-dark sm:border-t-0"
-			:class="[
-				open ? 'w-full flex justify-between' : '',
-				closing ? 'w-full flex justify-between' : '',
-			]"
-		>
-			<nuxt-link to="#" class="sm:ml-auto">Test</nuxt-link>
-			<nuxt-link to="#" class="ml-4">Test</nuxt-link>
-			<nuxt-link to="#" class="ml-4">Test</nuxt-link>
-		</div>
+		<transition name="navbar">
+			<div
+				class="navLinksTest sm:block sm:w-auto sm:transition-none border-t-2 border-dark sm:border-t-0 w-full flex justify-between"
+				v-if="open"
+			>
+				<nuxt-link to="#" class="sm:ml-auto">Test</nuxt-link>
+				<nuxt-link to="#" class="ml-4">Test</nuxt-link>
+				<nuxt-link to="#" class="ml-4">Test</nuxt-link>
+			</div>
+		</transition>
 	</nav>
 </template>
 
@@ -30,7 +29,6 @@ export default {
 	data() {
 		return {
 			open: true,
-			closing: false,
 		};
 	},
 
@@ -46,14 +44,9 @@ export default {
 	},
 	methods: {
 		mobileNav() {
+			console.log(this.open);
 			if (this.open) {
 				this.open = false;
-
-				this.closing = true;
-
-				setTimeout(() => {
-					this.closing = false;
-				}, 500);
 			} else {
 				this.open = true;
 			}
