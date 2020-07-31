518af230f41be4760773103c67f6c2ec3057dd26
Samuel <samuelvvelzen@gmail.com>
2020-07-31 09:55:44 +0200

    improving styling gallery

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -1,67 +1,65 @@
 <template>
-	<section
-		class="gallery flex overflow-x-hidden my-4 mx-0 sm:mx-8 md:mx-12 lg:mx-16 relative"
-		:style="cssProps"
-	>
-		<div class="images flex h-full w-full">
-			<img
-				v-for="n in galleryContent"
-				:key="n.alt"
-				class="h-full w-full"
-				:src="showImage(n.url)"
-				:alt="n.alt"
-			/>
-		</div>
-	</section>
+  <section
+    class="gallery flex overflow-x-hidden my-4 mx-4 sm:mx-12 md:mx-16 lg:mx-20 relative"
+    :style="cssProps"
+  >
+    <div class="images flex h-full w-full">
+      <img
+        v-for="n in galleryContent"
+        :key="n.alt"
+        class="h-full w-full"
+        :src="showImage(n.url)"
+        :alt="n.alt"
+      />
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-	props: {
-		galleryContent: {
-			type: Object,
-			default: () => {
-				return {};
-			},
-		},
-	},
-	computed: {
-		cssProps() {
-			return {
-				"--galleryLength":
-					"-" +
-					(Object.keys(this.galleryContent).length - 1) * 100 +
-					"%",
-				"--gallerySpeed":
-					Object.keys(this.galleryContent).length * 6 + "s",
-			};
-		},
-	},
-	methods: {
-		showImage(img) {
-			return require("../assets/" + img);
-		},
-	},
+  props: {
+    galleryContent: {
+      type: Object,
+      default: () => {
+        return {};
+      },
+    },
+  },
+  computed: {
+    cssProps() {
+      return {
+        "--galleryLength":
+          "-" + Object.keys(this.galleryContent).length * 100 + "%",
+        "--gallerySpeed": Object.keys(this.galleryContent).length * 6 + "s",
+      };
+    },
+  },
+  methods: {
+    showImage(img) {
+      return require("../assets/" + img);
+    },
+  },
 };
 </script>
 >
 
 <style lang="scss">
 .gallery {
-	background-color: white;
+  background-color: white;
 }
 
 .images {
-	animation: slideshow linear infinite normal;
-	animation-duration: var(--gallerySpeed);
+  animation: slideshow linear infinite normal;
+  animation-duration: var(--gallerySpeed);
+  transform: translateX(60%);
 }
 
 @keyframes slideshow {
-	0% {
-		transform: translateX(0);
-	}
-	100% {
-		transform: translateX(var(--galleryLength));
-	}
+  0% {
+    transform: translateX(60%);
+  }
+  100% {
+    transform: translateX(var(--galleryLength));
+  }
 }
 </style>
