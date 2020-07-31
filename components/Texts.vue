85f0ab39a5424302dc61abcddd7a713d1f183477
Samuel <samuelvvelzen@gmail.com>
2020-07-31 09:55:14 +0200

    make texts component with dynamic class

--- components/Texts.vue
+++ components/Texts.vue
@@ -1,18 +1,22 @@
 <template>
-	<section class="py-4 px-0 sm:px-8 md:px-12 lg:px-16">
-		<nuxt-content :document="texts" />
-	</section>
+  <section class="px-4 py-4 sm:px-12 md:px-16 lg:px-16" :class="textClass">
+    <nuxt-content :document="content" />
+  </section>
 </template>
 
 <script>
 export default {
-	props: {
-		texts: {
-			type: Object,
-			default: () => {
-				return {};
-			},
-		},
-	},
+  props: {
+    textClass: {
+      type: String,
+      default: "",
+    },
+    content: {
+      type: Object,
+      default: () => {
+        return {};
+      },
+    },
+  },
 };
 </script>
