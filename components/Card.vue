5296c35f5e35490dd07f352a16195f384ecc5d80
Samuel <samuelvvelzen@gmail.com>
2020-07-31 10:22:13 +0200

    renamed all primary colors to light

--- components/Card.vue
+++ components/Card.vue
@@ -1,19 +1,15 @@
 <template>
-	<div
-		class="bg-primary sm:rounded-md w-full lg:w-3/10 pb-8 sm:pb-4 sm:mb-8 lg:mb-0 last:mb-0"
-	>
-		<h2 class="text-xl font-bold text-center pb-3">
-			{{ title }}
-		</h2>
-		<p class="">{{ content }}</p>
-	</div>
+  <div class="bg-light sm:rounded-md w-full lg:w-3/10 pb-8 sm:pb-4 sm:mb-8 lg:mb-0 last:mb-0">
+    <h2 class="text-xl font-bold text-center pb-3">{{ title }}</h2>
+    <p class>{{ content }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-	props: {
-		title: { type: String, default: "test" },
-		content: { type: String, default: "test" },
-	},
+  props: {
+    title: { type: String, default: "test" },
+    content: { type: String, default: "test" },
+  },
 };
 </script>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,63 +1,60 @@
 <template>
-	<nav
-		class="fixed w-full flex justify-between bg-primary items-center py-2 px-4 sm:px-16 md:px-20 lg:px-16 lg:max-w-3/4 lg:mx-auto z-50 opacity-95 flex-wrap lg:left-1/8"
-		:class="open ? 'open' : null"
-	>
-		<nuxt-link to="/" class="text-2xl font-bold">Samuel.</nuxt-link>
-		<label
-			class="menu-icon sm:hidden cursor-pointer ml-auto pt-px-10 pb-px-10"
-			@click="mobileNav"
-		>
-			<span class="menu-icon-line block"></span>
-		</label>
+  <nav
+    class="fixed w-full flex justify-between bg-light items-center py-2 px-4 sm:px-16 md:px-20 lg:px-16 lg:max-w-3/4 lg:mx-auto z-50 opacity-95 flex-wrap lg:left-1/8"
+    :class="open ? 'open' : null"
+  >
+    <nuxt-link to="/" class="text-2xl font-bold">Samuel.</nuxt-link>
+    <label class="menu-icon sm:hidden cursor-pointer ml-auto pt-px-10 pb-px-10" @click="mobileNav">
+      <span class="menu-icon-line block"></span>
+    </label>
 
-		<transition name="navbar">
-			<div
-				class="navLinksTest sm:block sm:w-auto sm:transition-none border-t-2 border-dark sm:border-t-0 w-full flex justify-between"
-				v-if="open"
-			>
-				<nuxt-link to="#" class="sm:ml-auto">Test</nuxt-link>
-				<nuxt-link to="#" class="ml-4">Test</nuxt-link>
-				<nuxt-link to="#" class="ml-4">Test</nuxt-link>
-			</div>
-		</transition>
-	</nav>
+    <transition name="navbar">
+      <div
+        class="navLinksTest sm:block sm:w-auto sm:transition-none border-t-2 border-dark sm:border-t-0 w-full flex justify-between"
+        v-if="open"
+      >
+        <nuxt-link to="#" class="sm:ml-auto">Test</nuxt-link>
+        <nuxt-link to="#" class="ml-4">Test</nuxt-link>
+        <nuxt-link to="#" class="ml-4">Test</nuxt-link>
+      </div>
+    </transition>
+  </nav>
 </template>
 
 <script>
 export default {
-	data() {
-		return {
-			open: true,
-		};
-	},
+  data() {
+    return {
+      open: true,
+    };
+  },
 
-	mounted() {
-		if (window.innerWidth < 640) {
-			this.open = false;
-		}
+  mounted() {
+    if (window.innerWidth < 640) {
+      this.open = false;
+    }
 
-		window.addEventListener("resize", this.checkMobileNav);
-	},
-	destroyed() {
-		window.removeEventListener("resize", this.checkMobileNav);
-	},
-	methods: {
-		mobileNav() {
-			console.log(this.open);
-			if (this.open) {
-				this.open = false;
-			} else {
-				this.open = true;
-			}
-		},
-		checkMobileNav() {
-			if (window.innerWidth > 640) {
-				this.open = true;
-			} else {
-				this.open = false;
-			}
-		},
-	},
+    window.addEventListener("resize", this.checkMobileNav);
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.checkMobileNav);
+  },
+  methods: {
+    mobileNav() {
+      console.log(this.open);
+      if (this.open) {
+        this.open = false;
+      } else {
+        this.open = true;
+      }
+    },
+    checkMobileNav() {
+      if (window.innerWidth > 640) {
+        this.open = true;
+      } else {
+        this.open = false;
+      }
+    },
+  },
 };
 </script>
