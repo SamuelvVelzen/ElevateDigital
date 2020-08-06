52f63e6973b8f09f0f009d5debe87ac72a0358b4
Samuel <samuelvvelzen@gmail.com>
2020-08-06 09:55:41 +0200

    improve use of bootstrap components in navbar in favor of normal divs

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,18 +1,18 @@
 <template>
-	<b-navbar toggleable="sm" type="dark" variant="dark" :sticky="true">
-		<div class="container text-light">
-			<b-navbar-brand to="/">Samuel.</b-navbar-brand>
+	<b-navbar toggleable="sm" variant="dark" :sticky="true">
+		<b-container class="p-0">
+			<b-navbar-brand tag="h1" to="/">Samuel.</b-navbar-brand>
 
 			<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
 			<b-collapse id="nav-collapse" is-nav>
 				<b-navbar-nav class="ml-auto">
-					<b-nav-item to="/home">Homepagina</b-nav-item>
+					<b-nav-item to="/">Homepagina</b-nav-item>
 					<b-nav-item to="/timeline">Tijdlijn</b-nav-item>
 					<b-nav-item to="/login">Inloggen</b-nav-item>
 				</b-navbar-nav>
 			</b-collapse>
-		</div>
+		</b-container>
 	</b-navbar>
 </template>
 
