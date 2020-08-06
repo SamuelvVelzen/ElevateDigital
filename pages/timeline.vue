947d2105ff7c12e4fe47999e00192cd6daa2aeff
Samuel <samuelvvelzen@gmail.com>
2020-08-06 11:17:24 +0200

    added line and multiple items

--- pages/timeline.vue
+++ pages/timeline.vue
@@ -2,7 +2,26 @@
 	<article>
 		<b-container>
 			<h1>Tijdlijn</h1>
-			<timeline-item />
+
+			<div class="position-relative">
+				<span class="h-line"></span>
+				<timeline-item />
+				<timeline-item />
+				<timeline-item />
+			</div>
 		</b-container>
 	</article>
 </template>
+
+<style lang="scss" scoped>
+.h-line {
+	display: block;
+	position: absolute;
+	left: 12px;
+
+	height: 100%;
+	width: 2px;
+
+	background-color: var(--dark);
+}
+</style>
