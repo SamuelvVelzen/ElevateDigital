48cfdbb9196d48f061d564451b926d6263e502af
Samuel <samuelvvelzen@gmail.com>
2020-08-06 11:10:06 +0200

    added styling and layout of timelineitem

---
+++ components/TimelineItem.vue
@@ -0,0 +1,62 @@
+<template>
+	<b-container>
+		<b-row>
+			<b-col offset="2" offset-md="1" cols="10" md="11" class="p-0">
+				<h5 class="font-weight-bold mb-0">Titel</h5>
+			</b-col>
+			<b-col md="12" class="position-relative p-0">
+				<span class="circle-outer rounded-circle">
+					<span class="circle-inner rounded-circle"></span>
+				</span>
+				<span class="line"></span>
+			</b-col>
+			<b-col offset="2" offset-md="1" cols="10" md="11" class="p-0">
+				lorem ipsum lalallalalla lorem ipsum lalallalalla lorem ipsum
+				lalallalalla lorem ipsum lalallalalla lorem ipsum lalallalalla
+				lorem ipsum lalallalalla lorem ipsum lalallalalla lorem ipsum
+				lalallalalla
+			</b-col>
+		</b-row>
+	</b-container>
+</template>
+
+<style lang="scss" scoped>
+.circle- {
+	&outer {
+		z-index: 100;
+		display: block;
+		position: relative;
+
+		border: 2px solid var(--dark);
+		background-color: var(--light);
+
+		height: 25px;
+		width: 25px;
+	}
+
+	&inner {
+		display: block;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+
+		background-color: var(--dark);
+
+		height: 14px;
+		width: 14px;
+	}
+}
+
+.line {
+	display: block;
+	position: absolute;
+	width: calc(100% - 20px);
+	margin-right: 5px;
+	top: 50%;
+	transform: translateY(-50%);
+
+	height: 2px;
+	background-color: var(--dark);
+}
+</style>
