46f54d20ca2b7779a6c13cd730b6bf0baeb56b58
Samuel <samuelvvelzen@gmail.com>
2020-08-10 12:08:41 +0200

    add form structure to login and a way to go to register

--- pages/login.vue
+++ pages/login.vue
@@ -13,6 +13,50 @@
 				<b-button variant="success" class="mt-4 mt-sm-0 ml-sm-4">
 					Inloggen bij Google
 				</b-button>
+
+				<hr />
+
+				<b-form
+					@submit="onSubmit"
+					@reset="onReset"
+					v-if="reset"
+					class="w-100"
+				>
+					<b-form-group
+						id="input-group-1"
+						label="Username:"
+						label-for="input-1"
+					>
+						<b-form-input
+							id="input-1"
+							v-model="form.username"
+							required
+							placeholder="Enter username"
+							autocomplete="username"
+						></b-form-input>
+					</b-form-group>
+
+					<b-form-group
+						id="input-group-2"
+						label="Password:"
+						label-for="input-2"
+					>
+						<b-form-input
+							id="input-2"
+							v-model="form.password"
+							required
+							type="password"
+							placeholder="Enter password"
+							autocomplete="current-password"
+						></b-form-input>
+					</b-form-group>
+
+					<b-link to="/register">Register</b-link>
+					<b-button type="reset" variant="danger" class="ml-auto"
+						>Reset</b-button
+					>
+					<b-button type="submit" variant="primary">Login</b-button>
+				</b-form>
 			</b-container>
 		</b-container>
 	</article>
@@ -23,3 +67,36 @@
 	height: calc(100% - 56px);
 }
 </style>
+
+<script>
+export default {
+	data() {
+		return {
+			form: {
+				username: "",
+				password: "",
+			},
+
+			reset: true,
+		};
+	},
+	methods: {
+		onSubmit(evt) {
+			evt.preventDefault();
+			alert(JSON.stringify(this.form));
+		},
+		onReset(evt) {
+			evt.preventDefault();
+			// Reset our form values
+			this.form.username = "";
+			this.form.password = "";
+
+			// Trick to reset/clear native browser form validation state
+			this.reset = false;
+			this.$nextTick(() => {
+				this.show = true;
+			});
+		},
+	},
+};
+</script>
